f8d80a98a31c2e31b87f161ffc966d254a9af11d
674244121-ภัคพล เจียมจารีกุล <674244121@mail.pbru.ac.th>
2025-08-14 11:58:13 +0700

    Add files via upload

---
+++ OS-code/Untitled1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main() {
+	printf("My bio.: \n");
+	printf("ID : 674244121 \n");
+	printf("Name : phakkhaphon jeamjarikun\n");
+	printf("------------------------------\n");
+	getchar();
+}
